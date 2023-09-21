ba034081e02de9aae61e0dbdbb89626cf1145cdb
Hussam Qannam <hussam.a.qannam@gmail.com>
2023-09-22 02:28:22 +0300

    done

---
+++ dist/script.js
@@ -0,0 +1,54 @@
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+};
+
+const skillsText = document.getElementById("skills-text");
+const projectText = document.getElementById("project-text");
+const contactText = document.getElementById("contact-text");
+const gamesser = document.getElementById("gamesser");
+const keytab = document.getElementById("keytab");
+const pigeon = document.getElementById("pigeon");
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            if (entry.target.matches("#skills-text")) {
+                if (entry.isIntersecting) {
+                    skillsText.classList.add("move-left");
+                    observer.unobserve(skillsText);
+                    return;
+                }
+            }
+            if (entry.target.matches("#project-text")) {
+                if (entry.isIntersecting) {
+                    projectText.classList.add("move-down");
+                    observer.unobserve(projectText);
+                    return;
+                }
+            }
+            if (entry.target.matches("#contact-text")) {
+                if (entry.isIntersecting) {
+                    contactText.classList.add("move-down");
+                    observer.unobserve(contactText);
+                    return;
+                }
+            }
+            if (entry.target.matches("#keytab")) {
+                if (entry.isIntersecting) {
+                    keytab.classList.add("appear");
+                    gamesser.classList.add("move-right");
+                    pigeon.classList.add("move-left");
+                    observer.unobserve(keytab);
+                    return;
+                }
+            }
+        });
+    },
+    {
+        rootMargin: "-100px",
+    }
+);
+observer.observe(skillsText);
+observer.observe(projectText);
+observer.observe(contactText);
+observer.observe(keytab);
